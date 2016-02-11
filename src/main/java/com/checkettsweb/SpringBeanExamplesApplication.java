3b4049b6618a191a21c5c8a553bea69cd4afdc49
Clint Checketts <clint.checketts@domo.com>
2016-02-11 15:56:07 -0700

    Remove component scan

--- src/main/java/com/checkettsweb/SpringBeanExamplesApplication.java
+++ src/main/java/com/checkettsweb/SpringBeanExamplesApplication.java
@@ -1,17 +1,30 @@
 package com.checkettsweb;
 
+import com.checkettsweb.controller.Greeter;
+import com.checkettsweb.controller.HelloController;
+import com.checkettsweb.controller.InformalGreeter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
 public class SpringBeanExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBeanExamplesApplication.class, args);
+	}
+
+	@Bean
+	public HelloController helloController(Greeter greeter){
+		return new HelloController(greeter);
+	}
+
+	@Bean Greeter greeter(@Value("${greeting.informal}") String greeting){
+		return new InformalGreeter(greeting);
 	}
 }
